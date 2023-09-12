dddebcde04289e7c5d96ded8e8176fa1225f9fb0
Aditya-Jannawar <adijannawar@gmail.com>
2023-09-12 06:29:45 +0000

    changes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,76 @@
+// let score = 33
+// let scoreb = "33abc"
+// let bool = true
+// let nulll = null
+// // check always whether it is real numbaer 
+// console.log(typeof score); 
+// console.log(typeof (scoreb));
+
+// let valueInNumber = Number(scoreb)// Conversion : string to number
+
+// console.log(typeof (valueInNumber)); //Number
+
+// console.log(valueInNumber); // NaN
+// console.log(Number(bool));// 1 for T or 0 for false
+// console.log(Number(nulll));//0
+
+// /*
+// "33abc" => Nan but its number
+//  */
+
+// let isLoggedIn = 1
+// let study = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let boolstudy = Boolean(study)
+// console.log(booleanIsLoggedIn);
+// console.log(boolstudy);
+
+// /*
+//  converting to boolean
+//  1 => true ; 0=> false
+//  "" => false
+//  "adcdcd" => true
+//  */
+
+//  let num = 22
+//  let strnum = String(num)
+//  console.log(strnum);
+//  console.log(typeof strnum);
+
+
+ //-------------------operations-------------------------------
+
+ 
+ let val =3
+ let negval =-val
+ console.log(negval);
+
+ //power operator: 2**3 = 2 to the power 3
+ //modulo % 
+ 
+ console.log(2**3);
+
+ let str1 ="hello_"
+ let str2 = "aditya"
+
+ console.log(str1+str2);
+ console.log("1"+2);
+ // console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
